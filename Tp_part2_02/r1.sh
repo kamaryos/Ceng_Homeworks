96d30e7438283598d0b22e28b62c4bdb76d21417
kamaryos <merttuncay96@gmail.com>
2019-01-17 19:00:01 +0300

    Add files via upload

---
+++ Tp_part2_02/r1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo route add -host 10.10.2.1 gw 10.10.2.2
+
+sudo route add -host 10.10.3.2 gw 10.10.3.1
+
+route -n
+
+
---
+++ Tp_part2_02/r2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+sudo route add -host 10.10.4.1 gw 10.10.4.2
+
+sudo route add -host 10.10.5.2 gw 10.10.5.1
+
+route -n
+
+
