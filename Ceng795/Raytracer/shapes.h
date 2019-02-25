deb8279b44919ae75b9af4d93547af8642d574d3
kamaryos <merttuncay96@gmail.com>
2019-02-25 16:53:38 +0300

    Add files via upload

--- Ceng795/Raytracer/shapes.h
+++ Ceng795/Raytracer/shapes.h
@@ -35,6 +35,7 @@ public:
   float radius;
   Sphere(){}
   Sphere(int material_id, int center_vertex_id, float radius) : object_type(1),material_id(material_id), center_vertex_id(center_vertex_id), radius(radius) {}
+  Vec4f hit_sphere(const ray& r, const vec3& center, float radius);
 };
 
 class Triangle : public Shapes{
@@ -43,17 +44,65 @@ public:
   Triangle()
   Triangle(int material_id, Face indices) : object_type(2), material_id(material_id){
     indices = Face(indices.v0_id,indices.v1_id,indices.v2_id);
-  };
-
+    }
+  Vec4f hit_triangle(const ray& r, const &vec3 p1, const &vec3 p2, const &vec3 p3);
 };
 
 class Mesh : public Shapes{
 public:
   std::vector<Face> faces;
   Mesh()
-  Mesh(std::vector<Face> faces):faces(vector<Face> n_faces(faces)){} // Is this possible and the class architecture is ok or not!?
+  Mesh(std::vector<Face> faces):object_type(3), faces(vector<Face> n_faces(faces)){} // Is this possible and the class architecture is ok or not!?
 
 };
+
+void t_min(float t, float t1, float t2){
+  if(t1<t2){t=t1;}
+  else if(t2<t1){t=t2;}
+  else if(t1==t2){t=t1;}
+};
+
+
+Vec4f Sphere::hit_sphere(const ray& r, const vec3& center, float radius){ // if ray intersects, return intersection point, if not 0,0,0,-1
+
+  vec3 oc = (r.origin() - center);
+
+  float a = dot(r.direction(),r.direction());
+  float b = 2*dot(r.direction(),(r.direction() - center));
+  float c = dot(oc,oc) - (radius * radius);
+
+  float delta = (b*b) - (4*a*c);
+  float t = 0;
+
+  if(delta < 0){
+      //Find a better way to return this part
+        Vec4f result(0,0,0,-1);
+        return result;
+  }
+
+  else{
+
+      float t1 = ((-1)*b + sqrt(delta))/(2*a);
+      float t2 = ((-1)*b - sqrt(delta))/(2*a);
+      t_min(t,t1,t2);
+  }
+
+  //Vec3 --> Vec4f function need to be added
+
+  Vec4f result (r.origin()+t*r.direction(),t);
+  return result;
+};
+
+Vec4f Triangle::hit_triangle(const ray& r, const &vec3 p1, const &vec3 p2, const &vec3 p3){
+
+  
+
+
+};
+
+
+
+
 
 
 #endif
