82157155716c167afb14d64a0b7d65ca09c919f8
kamaryos <merttuncay96@gmail.com>
2019-03-13 17:30:03 +0300

    Add files via upload

--- 334/hw1.c
+++ 334/hw1.c
@@ -1,80 +1,146 @@
-/* parent master -> child slave (bc -q, a simple calculator)
-   two pipes one for parent to child, other from child to parent
-   parent -> 3+4, 5*6 * ... child bc -q results -> parent
-*/
-#include <unistd.h>
-#include <stdio.h>
-#include <fcntl.h>
-#include <sys/stat.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/wait.h>
+#include<unistd.h>
+#include<stdio.h>
 
-int main(int argc, char *argv[]) {
+//
+// #define _GNU_SOURCE
+// #include <stdio.h>
+// #include <stdlib.h>
+//
+// int main(void)
+// {
+//     FILE * fp;
+//     char * line = NULL;
+//     size_t len = 0;
+//     ssize_t read;
+//
+    // fp = fopen("/etc/motd", "r");
+    // if (fp == NULL)
+    //     exit(EXIT_FAILURE);
+//
+//     while ((read = getline(&line, &len, fp)) != -1) {
+//         printf("Retrieved line of length %zu:\n", read);
+//         printf("%s", line);
+//     }
+//
+//     fclose(fp);
+//     if (line)
+//         free(line);
+//     exit(EXIT_SUCCESS);
+// }
 
 
-	printf("I reached 17");
-	int p,i;
+void close_pipe(int **fd,int i,int N){
+	for(int j =0 ; j < N ; j++){
+		if(j != i){
+			close(fd[j][0]);
+			close(fd[j][1]);
+		}
+	}
+}
+
+void read_line(	);
+
+int main(int argc, int argv[]) {
+
+	int p,i,q;
 	int fd1[2],fd2[2];
-	char *messages[]={"3+4\n","5*6\n","10+100-(4*4)\n"};
-	char buffer[100];
-	char readbuffer[100];
+	char *messages[]={"5+4\n","5*6\n","23+100-(4*4)\n"};
 
-	printf("I reached 22");
+	if(argc < 3 ){
+		printf("Please enter the needed arguments");
+		exit(1);
+	}
+
+	else if(argc == 3){
+
+		int N = argv[2];
+
+		int **fd = (int **)malloc(N * sizeof(int *));;
+
+		for(int index; index < N ; index++){
+			fd[index] = (int *)malloc(2*sizeof(int));
+		}
+
+
+		FILE *fp;
+		char * line = NULL;
+		size_t len = 0;
+		ssize_t read;
+
+		fp = fopen("/etc/motd", "r");
+		if (fp == NULL)
+				exit(EXIT_FAILURE);
+
+		for(j = 0 ; j < N ; j++ ){
+			if(fork()){
+				close(fd[i][0]);
+				close_pipe(fd,i,N);
+				dup2(fd[i][1],1);
+			}
+			else{
+					close(fd[i][1]);
+					close_pipe(fd,i,N);
+					dup2(fd[i][0],0);
+			}
+		}
+
+	}
+	else if(argc == 4){
+	}
+	else{
+		printf("Too many argumants!!")
+		exit(2);
+	}
+
 
 	pipe(fd1);
 	pipe(fd2);
 
-	if (p=fork()) { //Parent process
+	if (p=fork()) {
 
-		close(fd2[0]); // Read end of the second pipe closed
-		close(fd2[1]); // Write end of the second pipe closed
-		close(fd1[0]); // Read end of the first pipe closed
+		if(q=fork()){
 
-		dup2(fd1[1],1); // Duplicate write of the first pipe to stdout
+			close(fd1[0]);
+			close(fd2[0]);
+			close(fd2[1]);
+			dup2(fd1[1],2);
+			close(fd1[0]);
 
-		printf("I reached 33");
+			for (i=0;i<3;i++) {
+				fprintf(stderr,messages[i]);
+				fflush(stderr);
+				sleep(1);
+			}
 
 
-		fd1[1] = open("./word_count/input/input.txt", O_RDONLY | O_WRONLY);
+		}
+		else{
+			close(fd1[1]);
+			close(fd2[0]);
+			dup2(fd1[0],0);
+			dup2(fd2[1],1);
+			close(fd1[0]);
+			close(fd2[1]);
+			execl("/usr/bin/bc","bc","-q",(char *)0);
 
-		if(fd1[1] < 0){
-			perror("Not opened"); exit(1);
+
 		}
 
-		while(i){
-				i = read(fd1[1],buffer,10);
-		}
 
-		printf("I reached 45");
-		write(fd1[1], buffer, (strlen(buffer) + 1));
-		close(fd1[1]); // Write end of the first pipe closed
-
-		waitpid(p,NULL,0);
-
-	}
-	else {
-
-		close(fd1[1]); // Write end of the first pipe closed
-		close(fd2[0]); // Read end of the second pipe closed
-
-    int nbytes = read(fd1[0], readbuffer, sizeof(readbuffer));
-
-		dup2(fd1[0],0); // Duplicate read end of the first pipe to stdin
-		dup2(fd2[1],1); // Duplicate write end of the second pipe to stdout
-
-		fd2[1] = open("/dev/pts/22", O_RDONLY | O_WRONLY);
-
-		printf("I reached ");
-
+	} else {
 		close(fd1[0]);
-		close(fd2[1]); // Write end of the second pipe closed
+		close(fd1[1]);
+		close(fd2[1]);
+		dup2(fd2[0],0);
+		close(fd2[0]);
 
-		execv("./word_count/src/WC_Mapper",readbuffer);
+		for(i=0;i<3;i++){
+			if (fgets(result,100,stdin)) {
+				printf("%s\n",result);
+			}
+		}
 
 	}
-
 
 	return 0;
 }
